95d1da69db792ebcffd109f1a5c0e1d5cbcc2896
Diyan Rahaman <diyan.rahaman@verizon.net>
2016-10-01 21:37:08 -0400

    Third commit

--- app.js
+++ app.js
@@ -31,6 +31,8 @@
 		itemsTobuy.push({name: "bags of cookies", quantity: "10"});
 		itemsTobuy.push({name: "cans of soda", quantity: "5"});
 		itemsTobuy.push({name: "bags of chips", quantity: "20"});
+		itemsTobuy.push({name: "cups of noodles", quantity: "2"});
+		itemsTobuy.push({name: "cans of soup", quantity: "3"});
 
 		var itemsBought = [];
 
